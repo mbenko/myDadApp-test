55ac729fd18807846238a3c96e1baa2822f9969a
Mike Benkovich <mike@benko.com>
2019-06-17 18:23:06 -0500

    Add chores to staging slot

--- myDadApp/Controllers/ChoresController.cs
+++ myDadApp/Controllers/ChoresController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using myDadApp.Models;
 
 namespace myDadApp.Controllers
 {
+    //[Authorize]
     public class ChoresController : Controller
     {
         private readonly myDataContext _context;
